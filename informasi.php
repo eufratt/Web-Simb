afd9882667efe3cb5bcdf9d37bfc2dc297e7ff42
tikodjati08@gmail.com <tikodjati08@gmail.com>
2025-11-25 09:47:32 +0700

    styling dan menambahkan video

---
+++ informasi.php
@@ -0,0 +1,100 @@
+<!-- Informasi: Apa Itu Tanah Longsor? -->
+<section id="info" class="py-20 bg-stone-700">
+    <div class="container mx-auto px-2 pt-1">
+        <div class="flex flex-col md:flex-row items-center gap-12">
+            <!-- Gambar/Ilustrasi -->
+            <div class="w-full md:w-1/2 animasi bg-white/10 backdrop-blur-3xl border border-white/20
+    shadow-lg rounded-2xl px-1 py-1 flex items-center justify-between">
+                <!-- <img src="" alt="Ilustrasi Tanah Longsor" class="rounded-lg shadow-2xl object-cover w-full h-100"> -->
+                <video autoplay muted loop playsinline class="w-full h-auto border rounded-xl drop-shadow-[0_0_40px_rgba(255,255,255,0.8)]">
+                    <source src="assets/VideoAnimasi.mp4" type="video/mp4">
+                </video>
+            </div>
+            <!-- Teks Penjelasan -->
+            <!-- <div class="w-ful md:w-1/2 overscroll-auto md:overscroll-contain overflow-y-auto h-80 p-2 bg-white/10 backdrop-blur-2xl border border-white/20 shadow-lg rounded-2xl px-6 text-white">
+
+                <h2 class="text-3xl md:text-4xl font-bold mb-6 mt-2">
+                    Definisi Tanah Longsor
+                </h2>
+
+                <p class="text-white text-lg mb-4">
+                    Tanah longsor adalah perpindahan material pembentuk lereng...
+                </p>
+
+                <p class="text-white text-lg mb-6">
+                    Peristiwa ini dapat terjadi secara tiba-tiba atau perlahan...
+                </p>
+
+                <h2 class="text-3xl md:text-4xl font-bold mb-6 mt-6">
+                    Faktor Penyebab Terjadinya Longsor
+                </h2>
+
+                <h4 class="text-2xl font-semibold mb-4">
+                    A. Faktor Alam
+                </h4>
+
+                <ol class="list-decimal ml-6 space-y-2 mb-6">
+                    <li>Perubahan kondisi geologi...</li>
+                    <li>Curah hujan tinggi...</li>
+                    <li>Topografi curam...</li>
+                </ol>
+
+                <h4 class="text-2xl font-semibold mb-4">
+                    B. Faktor Manusia
+                </h4>
+
+                <ol class="list-decimal ml-6 space-y-2 mb-8">
+                    <li>Penggundulan hutan...</li>
+                    <li>Pembangunan permukiman...</li>
+                    <li>Sistem drainase buruk...</li>
+                </ol>
+
+                <h2 class="text-3xl md:text-4xl font-bold mb-6">
+                    Tipe-Tipe Tanah Longsor
+                </h2>
+
+                <ul class="list-disc ml-6 space-y-2 mb-8">
+                    <li><strong>Falls</strong> – material batu...</li>
+                    <li><strong>Slides</strong> – pergerakan tanah...</li>
+                    <li><strong>Flows</strong> – tanah bergerak...</li>
+                    <li><strong>Topples</strong> – tanah atau batuan...</li>
+                    <li><strong>Creep</strong> – pergerakan lambat...</li>
+                    <li><strong>Spread</strong> – penyebaran lateral...</li>
+                </ul>
+
+                <h2 class="text-3xl md:text-4xl font-bold mb-6">
+                    Dampak Tanah Longsor
+                </h2>
+
+                <h4 class="text-2xl font-semibold mb-4">
+                    Dampak Positif
+                </h4>
+
+                <ul class="list-disc ml-6 space-y-2 mb-6">
+                    <li>Meningkatkan kestabilan tanah...</li>
+                    <li>Membentuk lahan baru...</li>
+                    <li>Membantu proses pelapukan batuan...</li>
+                    <li>Meningkatkan kelembapan tanah...</li>
+                </ul>
+
+                <h4 class="text-2xl font-semibold mb-4">
+                    Dampak Negatif
+                </h4>
+
+                <ul class="list-disc ml-6 space-y-2 mb-10">
+                    <li>Kerusakan lingkungan...</li>
+                    <li>Kerugian materi...</li>
+                    <li>Gangguan ekonomi...</li>
+                    <li>Risiko korban jiwa...</li>
+                </ul>
+
+                <div class="bg-white/20 border border-white/40 text-white p-2 rounded-lg text-center">
+                    <p class="font-medium">Memahami risiko di sekitar kita adalah langkah awal untuk keselamatan.</p>
+                </div> -->
+
+            </div>
+
+
+        </div>
+    </div>
+</section>
--- landing.php
+++ landing.php
@@ -125,11 +125,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <!-- HERO SLIDER -->
     <section id="beranda" class="relative min-h-screen flex items-center justify-start overflow-hidden hero-align">
         <div class="absolute inset-0">
-            <img src="assets/longsor2.jpg" alt="" class="w-full h-full object-cover opacity-80">
+            <img src="assets/image.png" alt="" class="w-full h-full object-cover opacity-80">
             <div class="absolute inset-0 bg-black/40"></div>
         </div>
 
-        <div class="relative z-10 text-left max-w-xl brightness-200 md:brightness-200">
+        <div class="relative z-10 text-left max-w-xl brightness-200 md:brightness-200 text-white font-bold drop-shadow-[0_0_40px_rgba(255,255,255,0.8)]">
 
             <h1 class="text-5xl md:text-7xl font-bold font-serif tracking-wide text-white mb-4 leading-tight">
                 Bencana <span class="text-brand-brown">Longsor</span>
@@ -155,102 +155,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 
 
-    <!-- Informasi: Apa Itu Tanah Longsor? -->
-    <section id="info" class="py-20 bg-stone-700">
-        <div class="container mx-auto px-6">
-            <div class="flex flex-col md:flex-row items-center gap-12">
-                <!-- Gambar/Ilustrasi -->
-                <div class="w-full md:w-1/2 animasi">
-                    <img src="" alt="Ilustrasi Tanah Longsor" class="rounded-lg shadow-2xl object-cover w-full h-100">
-                </div>
-                <!-- Teks Penjelasan -->
-                <div class="w-ful md:w-1/2 overscroll-auto md:overscroll-contain overflow-y-auto h-80 p-2 bg-white/10 backdrop-blur-2xl border border-white/20 shadow-lg rounded-2xl px-6 text-white">
-
-                    <h2 class="text-3xl md:text-4xl font-bold mb-6 mt-2">
-                        Definisi Tanah Longsor
-                    </h2>
-
-                    <p class="text-white text-lg mb-4">
-                        Tanah longsor adalah perpindahan material pembentuk lereng...
-                    </p>
-
-                    <p class="text-white text-lg mb-6">
-                        Peristiwa ini dapat terjadi secara tiba-tiba atau perlahan...
-                    </p>
-
-                    <h2 class="text-3xl md:text-4xl font-bold mb-6 mt-6">
-                        Faktor Penyebab Terjadinya Longsor
-                    </h2>
-
-                    <h4 class="text-2xl font-semibold mb-4">
-                        A. Faktor Alam
-                    </h4>
-
-                    <ol class="list-decimal ml-6 space-y-2 mb-6">
-                        <li>Perubahan kondisi geologi...</li>
-                        <li>Curah hujan tinggi...</li>
-                        <li>Topografi curam...</li>
-                    </ol>
-
-                    <h4 class="text-2xl font-semibold mb-4">
-                        B. Faktor Manusia
-                    </h4>
-
-                    <ol class="list-decimal ml-6 space-y-2 mb-8">
-                        <li>Penggundulan hutan...</li>
-                        <li>Pembangunan permukiman...</li>
-                        <li>Sistem drainase buruk...</li>
-                    </ol>
-
-                    <h2 class="text-3xl md:text-4xl font-bold mb-6">
-                        Tipe-Tipe Tanah Longsor
-                    </h2>
-
-                    <ul class="list-disc ml-6 space-y-2 mb-8">
-                        <li><strong>Falls</strong> – material batu...</li>
-                        <li><strong>Slides</strong> – pergerakan tanah...</li>
-                        <li><strong>Flows</strong> – tanah bergerak...</li>
-                        <li><strong>Topples</strong> – tanah atau batuan...</li>
-                        <li><strong>Creep</strong> – pergerakan lambat...</li>
-                        <li><strong>Spread</strong> – penyebaran lateral...</li>
-                    </ul>
-
-                    <h2 class="text-3xl md:text-4xl font-bold mb-6">
-                        Dampak Tanah Longsor
-                    </h2>
-
-                    <h4 class="text-2xl font-semibold mb-4">
-                        Dampak Positif
-                    </h4>
-
-                    <ul class="list-disc ml-6 space-y-2 mb-6">
-                        <li>Meningkatkan kestabilan tanah...</li>
-                        <li>Membentuk lahan baru...</li>
-                        <li>Membantu proses pelapukan batuan...</li>
-                        <li>Meningkatkan kelembapan tanah...</li>
-                    </ul>
-
-                    <h4 class="text-2xl font-semibold mb-4">
-                        Dampak Negatif
-                    </h4>
-
-                    <ul class="list-disc ml-6 space-y-2 mb-10">
-                        <li>Kerusakan lingkungan...</li>
-                        <li>Kerugian materi...</li>
-                        <li>Gangguan ekonomi...</li>
-                        <li>Risiko korban jiwa...</li>
-                    </ul>
-
-                    <div class="bg-white/20 border border-white/40 text-white p-2 rounded-lg text-center">
-                        <p class="font-medium">Memahami risiko di sekitar kita adalah langkah awal untuk keselamatan.</p>
-                    </div>
-
-                </div>
-
-
-            </div>
-        </div>
-    </section>
+    <?php include 'informasi.php' ?>
 
     <!-- Upaya Mitigasi -->
     <section id="mitigasi" class="py-20 bg-brand-light">
